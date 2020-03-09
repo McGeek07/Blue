21392a7e3edb90fce6a9103e414a3077dbc82dd1
McGeek07 <s.mcgeek07@gmail.com>
2020-03-09 09:23:53 -0600

    0.0.33

--- Blue/src/blue/Blue.java
+++ Blue/src/blue/Blue.java
@@ -7,7 +7,7 @@ import blue.util.Version;
 
 public class Blue {
 	public static final Version
-		VERSION = new Version("Blue", 0, 0, 32);
+		VERSION = new Version("Blue", 0, 0, 33);
 	
 	public static void main(String[] args) {
 		System.out.println(VERSION);
