bd57fc789c44f0ad0c77c01bc7cef954aac9a814
Shawn McGee <s.mcgeek07@gmail.com>
2020-05-07 10:41:30 -0600

    0.1.11

--- Blue/src/blue/Blue.java
+++ Blue/src/blue/Blue.java
@@ -1,19 +1,14 @@
 package blue;
 
 import blue.core.Engine;
-import blue.core.Sound;
 import blue.util.Version;
 
 public class Blue {
 	public static final Version
-		VERSION = new Version("Blue", 0, 1, 10);
+		VERSION = new Version("Blue", 0, 1, 11);
 	
 	public static void main(String[] args) {
-		System.out.println(VERSION);
-		
-		Sound sound = Sound.load("itb", "itb.wav");
-		sound.play();
-		
+		System.out.println(VERSION);		
 		Engine.init();
 	}
 }
