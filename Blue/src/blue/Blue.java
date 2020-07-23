a69cccfc55c7594a3fca64db8d88ddaa355148a7
Shawn McGee <s.mcgeek07@gmail.com>
2020-07-22 22:23:17 -0600

    0.2.4

--- Blue/src/blue/Blue.java
+++ Blue/src/blue/Blue.java
@@ -6,10 +6,11 @@ import blue.util.Version;
 
 public class Blue {
 	public static final Version
-		VERSION = new Version("Blue", 0, 2, 3);
+		VERSION = new Version("Blue", 0, 2, 4);
 	
 	public static void main(String[] args) {
 		Debug.info(new Object() {}, VERSION);
+		
 		Engine.init();
 	}
 }
---
+++ Blue/src/module-info.java
@@ -0,0 +1,9 @@
+module blue {
+	exports blue;
+	exports blue.core;
+	exports blue.math;
+	exports blue.util;
+	exports blue.util.event;
+	
+	requires transitive java.desktop;
+}
