3169d3b4e414e1312307346e21c09bf8fb6da29f
Shawn McGee <s.mcgeek07@gmail.com>
2020-07-23 19:58:20 -0600

    0.2.15

--- Blue/src/blue/Blue.java
+++ Blue/src/blue/Blue.java
@@ -1,6 +1,8 @@
 package blue;
 
 import blue.core.Engine;
+import blue.core.Module.Metrics;
+import blue.core.Stage;
 import blue.util.Debug;
 import blue.util.Version;
 
@@ -8,8 +10,11 @@ public class Blue {
 	public static final Version
 		VERSION = new Version("Blue", 0, 2, 15);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Debug.info(new Object() { /* trace */ }, VERSION);
+		
+		Stage.setProperty(Stage.DEBUG, Metrics.STAGE_METRICS);
+		
 		Engine.init();
 	}
 }
